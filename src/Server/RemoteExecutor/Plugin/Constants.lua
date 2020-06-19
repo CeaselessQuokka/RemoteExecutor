997e4f689c22a504432aa3a6a8ca46285ca6eb0c
CeaselessQuokka <hurricanedavensb@gmail.com>
2020-06-19 18:03:33 -0400

    Created Constants file

---
+++ src/Server/RemoteExecutor/Plugin/Constants.lua
@@ -0,0 +1,16 @@
+--- Constants
+-- @created 5/4/2020
+-- @edited 5/4/2020
+-- @usage A list of constants the plugin uses.
+
+--- Constants ---
+return {
+	-- Plugin Keys
+	IGNORE_LIST_KEY = "QDT_RemoteExecutor_IgnoreList_V2",
+	PLUGIN_SETTINGS_KEY = "QDT_RemoteExecutor_PLUGIN_V2",
+	PRESETS_SETTINGS_KEY = "QDT_RemoteExecutor_PRESETS_V2",
+
+	-- Colors
+	GUI_SELECTED_COLOR = Color3.new(0.125, 0.75, 0.125),
+	GUI_DESELECTED_COLOR = Color3.new(0, 0, 0)
+}
